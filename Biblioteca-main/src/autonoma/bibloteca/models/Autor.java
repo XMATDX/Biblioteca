c757db9260e89f83436a342b5e5fc638b75f64b0
mateo <mateoq152006@gmail.com>
2025-03-18 22:03:24 -0500

    Se modifica la clase autor

--- Biblioteca-main/src/autonoma/bibloteca/models/Autor.java
+++ Biblioteca-main/src/autonoma/bibloteca/models/Autor.java
@@ -1,22 +1,25 @@
-package autonoma.bibloteca.models;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-
-import autonoma.bibloteca.models.Persona;
+package proyecto.biblioteca.models;
 
 /**
  *
- * @author SOFIA RUDAS
+ * @author Mateo Quintero
  */
 public class Autor extends Persona {
     
+
+     ////////////////////////////////////////////////////////////////////////////////////
+     /// Atributos
+     /// 
     private String editorial;
     private String profesion;
     
+
+     ////////////////////////////////////////////////////////////////////////////////////
+     /// Metodo Contructor
     public Autor(String nombre, String documentoIdentidad, String correoElectronico, String editorial, String profesion) {
         
         super(nombre, documentoIdentidad, correoElectronico); //llamar al constructor de la clase Persona
